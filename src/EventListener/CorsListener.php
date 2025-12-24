5fbd6988f175e4b9642472aece1b577584422381
Tapha21 <tvphvtall@gmail.com>
2025-12-24 13:26:04 +0000

    version final

--- src/EventListener/CorsListener.php
+++ src/EventListener/CorsListener.php
@@ -8,9 +8,33 @@ class CorsListener
 {
     public function onKernelResponse(ResponseEvent $event): void
     {
+        $request = $event->getRequest();
         $response = $event->getResponse();
-        $response->headers->set('Access-Control-Allow-Origin', 'http://localhost:4200');
-        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+        $allowedOrigins = [
+            'http://localhost:4200',
+            'https://gestiondestache.vercel.app',
+        ];
+
+        $origin = $request->headers->get('Origin');
+
+        if ($origin && in_array($origin, $allowedOrigins, true)) {
+            $response->headers->set('Access-Control-Allow-Origin', $origin);
+        }
+
+        $response->headers->set(
+            'Access-Control-Allow-Methods',
+            'GET, POST, PUT, DELETE, OPTIONS'
+        );
+
+        $response->headers->set(
+            'Access-Control-Allow-Headers',
+            'Content-Type, Authorization'
+        );
+
+        $response->headers->set(
+            'Access-Control-Allow-Credentials',
+            'true'
+        );
     }
 }
