637017ec651bce3cab0cb4f5825e6a9f9da686c9
Tapha21 <tvphvtall@gmail.com>
2026-01-07 12:50:04 +0000

    version 7e

--- config/bundles.php
+++ config/bundles.php
@@ -12,6 +12,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true],
 
 ];
